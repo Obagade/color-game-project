01dc1f5a2c03f56054ed0944461b1ba2d6089744
Obagade <rotimiobags@gmail.com>
2020-06-03 21:35:20 +0200

    finalized the interactivity

--- colorGame.js
+++ colorGame.js
@@ -1,6 +1,5 @@
-var colors = generateRandomColors(6)
-   
-
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);  
 var squares = document.querySelectorAll(".square");
 var pickedColor= pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
@@ -10,10 +9,12 @@ var resetButton = document.querySelector("#reset");
 var easyBtn = document.querySelector("#easyBtn");
 var hardBtn = document.querySelector("#hardBtn");
 
+
 easyBtn.addEventListener("click", function(){
     hardBtn.classList.remove("selected");
     easyBtn.classList.add("selected");
-    colors = generateRandomColors(3);
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
     for(var i = 0; i < squares.length; i++){
@@ -29,19 +30,33 @@ hardBtn.addEventListener("click", function(){
     
     hardBtn.classList.add("selected");
     easyBtn.classList.remove("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++){
+            squares[i].style.backgroundColor = colors[i];
+            squares[i].style.display = "block";
+        }
+    
     
 });
 
 resetButton.addEventListener("click", function(){
 
+    colors = generateRandomColors(numSquares);
+
     pickedColor = pickColor();
 
     colorDisplay.textContent = pickedColor;
+    this.textContent = "New Colors"
+
+    messageDisplay.textContent = "";
 
     for(var i = 0; i < squares.length; i++){
         squares[i].style.background = colors[i];
     }
-    h1.style.background = "#232323";
+    h1.style.background = "steelblue";
 })
 
 colorDisplay.textContent = pickedColor;
